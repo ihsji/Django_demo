15deb3206b5a510bea9197b631bc3199f6b55a46
langyu233 <qjwfgg@163.com>
2024-09-13 02:54:12 +0800

    Revert "Add files via upload"
    This reverts commit da1e7113750fb78ad8f534c8ef28ca897119ce2b.

--- demo/app01/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path,re_path
-from app01.views import helloworld,article_create,article_detail,phone_detail
-urlpatterns = [
-    path("hello/",helloworld),
-    path("create/",article_create,name="article_create"),
-    path("<int:article_id>/<str:title>",article_detail,name="article_detail"),
-    re_path("(?P<phone_number>1[3456789]\d{9})/$",phone_detail,name="phone_detail"),
-]
